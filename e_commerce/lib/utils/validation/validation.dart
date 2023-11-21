6a1e2ff509d7f78ff80c0b8e0944fe98e7777257
Daniel Tilahun <tiledan2015@gmail.com>
2023-11-21 17:24:53 +0300

    adding phone number and name validation

--- e_commerce/lib/utils/validation/validation.dart
+++ e_commerce/lib/utils/validation/validation.dart
@@ -22,6 +22,44 @@ class Validator {
       return 'Password must be at least 6 characters';
     }
 
+    // Regular expressions for letters, numbers, and special characters
+    RegExp letterRegex = RegExp(r'[a-zA-Z]');
+    RegExp digitRegex = RegExp(r'\d');
+    RegExp specialCharRegex = RegExp(r'[!@#$%^&*(),.?":{}|<>]');
+
+    if (!letterRegex.hasMatch(value)) {
+      return 'Password must contain at least one letter';
+    }
+
+    if (!digitRegex.hasMatch(value)) {
+      return 'Password must contain at least one number';
+    }
+
+    if (!specialCharRegex.hasMatch(value)) {
+      return 'Password must contain at least one special character';
+    }
+
+    return null;
+  }
+
+  static String? validateName(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Name is required';
+    }
+
+    return null;
+  }
+
+  static String? validatePhoneNumber(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Phone number is required';
+    }
+
+    final phoneRegExp = RegExp(r'^[0-9]+$');
+    if (!phoneRegExp.hasMatch(value)) {
+      return 'Please enter a valid phone number';
+    }
+
     return null;
   }
 }
