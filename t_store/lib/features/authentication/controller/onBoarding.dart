325db7528acb0adc516b9649ce7dae8d22ed13ca
Daniel Tilahun <tiledan2015@gmail.com>
2023-11-21 20:06:13 +0300

    adding splash screen and finishing setup

---
+++ t_store/lib/features/authentication/controller/onBoarding.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+class OnBoardingController extends GetxController {
+  static OnBoardingController get instance => Get.find();
+
+  // variable
+
+  void updatePageIndicator(index) {}
+  void dotNavigationClick(index) {}
+  void nextPage() {}
+  void skipPage() {}
+}
