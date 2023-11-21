82498b4b961073fe19642535eb3c3a88b1e1f506
Daniel Tilahun <tiledan2015@gmail.com>
2023-11-21 12:09:16 +0300

    changeing variable name and text string

---
+++ e_commerce/lib/utils/constants/textString.dart
@@ -0,0 +1,18 @@
+class TextString {
+  // onBoarding
+  static const String onBoardingTitle1 = "Welcome to";
+  static const String onBoardingTitle2 = "E-Commerce";
+  static const String onBoardingTitle3 = "App";
+
+  // onBoarding sub title
+  static const String onBoardingSubTitle1 =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget aliquam quam. Donec eget nibh et magna sagittis aliquam.";
+  static const String onBoardingSubTitle2 =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget aliquam quam. Donec eget nibh et magna sagittis aliquam.";
+  static const String onBoardingSubTitle3 =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget aliquam quam. Donec eget nibh et magna sagittis aliquam.";
+
+  //  home app bar title and subtitle
+  static const String homeAppBarTitle = "E-Commerce";
+  static const String homeAppBarSubTitle = "App";
+}
