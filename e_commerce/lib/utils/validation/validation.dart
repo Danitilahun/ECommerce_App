fbbdcbd4b0aed3140fdf488419f980fb5c6f866b
Daniel Tilahun <tiledan2015@gmail.com>
2023-11-21 17:21:06 +0300

    validation for email and password added

---
+++ e_commerce/lib/utils/validation/validation.dart
@@ -0,0 +1,27 @@
+class Validator {
+  static String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Email is required';
+    }
+
+    final emailRegExp =
+        RegExp(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$');
+    if (!emailRegExp.hasMatch(value)) {
+      return 'Please enter a valid email address';
+    }
+
+    return null;
+  }
+
+  static String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Password is required';
+    }
+
+    if (value.length < 6) {
+      return 'Password must be at least 6 characters';
+    }
+
+    return null;
+  }
+}
