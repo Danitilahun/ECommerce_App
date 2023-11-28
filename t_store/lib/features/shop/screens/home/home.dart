3f041800f98b29dd0b85dc08c9bb5d0195ae6c54
Daniel Tilahun <tiledan2015@gmail.com>
2023-11-28 11:22:27 +0300

    adding custom search bar

--- t_store/lib/features/shop/screens/home/home.dart
+++ t_store/lib/features/shop/screens/home/home.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:t_store/common/widgets/appBar/appBar.dart';
 import 'package:t_store/common/widgets/custom_shape/CircularContainer.dart';
 import 'package:t_store/common/widgets/custom_shape/CurvedEdgeWidget.dart';
+import 'package:t_store/common/widgets/custom_shape/CustomSearchBar.dart';
 import 'package:t_store/common/widgets/custom_shape/curved_shapes.dart';
 import 'package:t_store/common/widgets/productCart/CartCountIcon.dart';
 import 'package:t_store/features/shop/screens/home/widgets/HomeAppBar.dart';
 import 'package:t_store/features/shop/screens/home/widgets/PrimaryHeaderComponet.dart';
 import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/constants/text_strings.dart';
+import 'package:t_store/utils/device/device_utility.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -22,6 +27,12 @@ class HomeScreen extends StatelessWidget {
             child: Column(
               children: [
                 HomeAppBar(),
+                const SizedBox(
+                  height: TSizes.spaceBtwSections,
+                ),
+                CustomSearchBar(
+                  text: "Search in store",
+                )
               ],
             ),
           ),
